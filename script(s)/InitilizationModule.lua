b09aacac4116ea15eff02d85501b56c74b16e013
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-09-25 07:35:19 +0100

    Update InitilizationModule.lua

--- script(s)/InitilizationModule.lua
+++ script(s)/InitilizationModule.lua
@@ -6,6 +6,51 @@ Init.Execute = function(Script)
    loadstring(game:HttpGet("https://raw.githubusercontent.com/misanthropic2005/DaHood/main/script(s)/" .. Script .. ".lua"))()
 end
 
+Init.ChangeValue = function(NewValue, Type, String, Alt)
+   
+   local Table = _G.MetaWareSettings
+   
+   if Type == 1 or Type > 1 then
+      
+      if Table["KeyBinds"] ~= nil then
+         
+         local Keys = Table["KeyBinds"]
+         
+         if Keys[String] ~= nil then
+            return Enum.KeyCode[Keys[String]]
+         else
+            return false
+         end
+      else
+         return false
+      end
+   else
+      if Type == 0 then
+         if Table["Booleans"] ~= nil then
+            
+            local Bools = Table["Booleans"]
+            
+            if Bools[Alt] ~= nil then
+               
+               local AltTable = Bools[Alt]
+               
+               if AltTable[String] ~= nil then
+                  return AltTable[String]
+               else
+                  return false
+               end
+            else
+               return false
+            end
+         else
+          return false
+       end
+   else
+      return false
+   end
+end
+end
+
 Init.GetValue = function(Table, Type, String, Alt)
    if Type == 1 or Type > 1 then
       
