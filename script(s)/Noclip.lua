8e0a8e93ea33b57f826958765368babbc0f130cf
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-09-25 07:25:25 +0100

    Create Noclip.lua

---
+++ script(s)/Noclip.lua
@@ -0,0 +1,37 @@
+local in_lib = "https://raw.githubusercontent.com/misanthropic2005/DaHood/main/script(s)/InitilizationModule.lua" --- init
+local pm_lib = "https://raw.githubusercontent.com/misanthropic2005/DaHood/main/script(s)/PlayerModule.lua" -- player
+local kb_lib = "https://raw.githubusercontent.com/misanthropic2005/DaHood/main/library(s)/KeyLIB_Custom.lua" -- bind
+
+if game:HttpGet(in_lib) then
+    in_lib = loadstring(game:HttpGet(in_lib))()
+else
+    game:GetService("Players").LocalPlayer:Kick("Fly failed to init[alize]")
+end
+
+if game:HttpGet(pm_lib) then
+    pm_lib = loadstring(game:HttpGet(pm_lib))()
+else
+    game:GetService("Players").LocalPlayer:Kick("Fly failed to init[alize]")
+end
+
+if game:HttpGet(kb_lib) then
+    kb_lib = loadstring(game:HttpGet(kb_lib))()
+else
+    game:GetService("Players").LocalPlayer:Kick("Fly failed to init[alize]")
+end
+
+kb_lib.ReturnBegan:Connect(function(I, G)
+    if I.KeyCode == in_lib.GetValue(_G.MetaWareSettings, 1, "NoclipBind") then
+       in_lib.GetValue(_G.MetaWareSettings, 0, "NoclipToggled", "Combat") = not in_lib.GetValue(_G.MetaWareSettings, 0, "NoclipToggled", "Combat")
+    end
+end)
+
+game:GetService("RunService").RenderStepped:Connect(function(...)
+    if in_lib.GetValue(_G.MetaWareSettings, 0, "NoclipToggled", "Combat") then
+       if pm_lib.Character() tthen       
+          for z, x in pairs(pm_lib.Character():GetChildren()) do
+             if x:IsA("BasePart") then x.CanCollide = false end
+          end
+       end
+    end
+end)
