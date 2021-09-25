0b976761b0d7916dc92977be317ba73d744b749f
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-09-25 05:19:26 +0100

    Update and rename Fly.lua to InitilizationModule.lua

---
+++ script(s)/InitilizationModule.lua
@@ -0,0 +1,51 @@
+--[[ UNIT 01 ]]--
+
+local Init = { }
+
+function Init.Execute(Script)
+   loadstring(game:HttpGet("" .. Script .. ".lua"))()
+end
+
+function Init.GetValue(Table, Type, String, Alt)
+   if Type == 1 or Type > 1 then
+      
+      if Table["KeyBinds"] ~= nil then
+         
+         local Keys = Table["KeyBinds"]
+         
+         if Keys[String] ~= nil then
+            return Enum.KeyCode[Keys[String]]
+         else
+            return false
+         end
+      else
+         return false
+      end
+   else
+      if Type == 0 then
+         if Table["Booleans"] ~= nil then
+            
+            local Bools = Table["Booleans"]
+            
+            if Bools[Alt] ~= nil then
+               
+               local AltTable = Bools[Alt]
+               
+               if AltTable[String] ~= nil then
+                  return AltTable[String]
+               else
+                  return false
+               end
+            else
+               return false
+            end
+         else
+            return false
+         end
+      else
+         return false
+      end
+   else
+      return false
+   end
+end
